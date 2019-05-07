3a6431b4687dea8850d8a437f211a860bc521789
Darren Dhanpat <darren.dhanpat@yahoo.com>
2019-05-06 23:44:38 -0400

    adding additional info adv javaScript

--- Adv JavaScript/script.js
+++ Adv JavaScript/script.js
@@ -1,5 +1,25 @@
-var b = "Can I access this";
+var fun = 5;
 
-function bb(){
-	b = "hello";
+function funFunction() {
+	//child scope
+	var fun = "helllo";
+	console.log(1, fun);
 }
+
+function funerFunction() {
+	//child scope
+	var fun = " bye";
+	console.log(2, fun);
+}
+
+
+function funestFunction() {
+	//child scope
+	fun = " aAHHH";
+	console.log(3, fun);
+}
+
+console.log("window", fun);
+funFunction();
+funerFunction();
+funestFunction();
