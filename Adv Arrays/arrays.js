f97eaae4dd721e3db9cc57aed8c18f65b902473a
Darren Dhanpat <darren.dhanpat@yahoo.com>
2019-05-15 23:30:30 -0400

    add adv arrays

--- Adv Arrays/arrays.js
+++ Adv Arrays/arrays.js
@@ -15,9 +15,21 @@ console.log(double);
 ///important methods for arrays
 //map, filter, reduce
 
-//map
+//map transforms the mapArray variable to an array
+//and because using .map you always have add a return statement when using map
 const mapArray = array.map((num) => {
 	return num * 2;
 });
 
-console.log(mapArray);
+console.log('map', mapArray);
+
+
+//filter allows you to use a condition and you must also use a return statement
+const filterArray = array.filter(num => {
+	return num  == 10
+});
+console.log('filter', filterArray);
+
+
+
+//reduce
