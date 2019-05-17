adc658dcdf881b78f422c8be6a1234eab6020700
Darren Dhanpat <darren.dhanpat@yahoo.com>
2019-05-17 08:50:08 -0400

    adding to adv objects

--- Adv Objects/objects.js
+++ Adv Objects/objects.js
@@ -6,7 +6,23 @@ var object3 = { value: 10};
 
 
 
-//context
+//context vs scope
+//note that 'this' is part of the current object that it lives in
+//ie: if you are creating an object let's say a car and you create a constructor; 
+//when you add values ex: for the color you would validate it with this.color = color;
+//the this.color refers to the current object of the car.
+function a() {
+	console.log(this);
+}
+a(); //this will just refer to the window object because a new object has not been created
+//so the default object is the window object
+
+const object4 = {
+	a: function() {
+		console.log(this);
+	}
+}
+
 
 //instantiation
 
