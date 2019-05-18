b13d4b656a7c771beb44b29fa2030f02e1967a72
Darren Dhanpat <darren.dhanpat@yahoo.com>
2019-05-18 02:04:46 -0400

    adding to adv loops

---
+++ Adv Objects/ex7.js
@@ -0,0 +1,12 @@
+//includes method
+'hellooo'.includes('o') //this will be true
+//the includes method checks if the value in the parenthses is in the word
+
+//it also works with arrays
+const pets = ['dog', 'bird', 'bat'];
+pets.includes('dog'); //will be true
+
+
+//exponential operator
+const square = (x) => x**2
+square(5); //25
